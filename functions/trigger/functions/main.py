0807f76ff6e0ba56632cdc06095a9e2431772d14
jokau7 <jonas.kauke1@ingka.com>
2025-06-23 18:00:20 +0200

    fix prompt

--- functions/trigger/functions/main.py
+++ functions/trigger/functions/main.py
@@ -5,48 +5,55 @@ from firebase_functions.firestore_fn import (
     DocumentSnapshot,
 )
 
-# from vertexai import agent_engines
-from google import auth as google_auth
-from google.auth.transport import requests as google_requests
-
-# import requests
-# import json
-import pprint
-
 import vertexai
 from vertexai import agent_engines
 
 project_id = "hacker2025-team-182-dev"
-agent_engine_resource_id = "9130379741423992832"
 agent_engine_resource_location = "europe-west1"
 agent_engine_app_name = "Aurora Recruiting Agent"
 agent_engine_bucket_name = "project-aurora-dev-bucket"
-
-reasoning_engine_resource_id = f"projects/{project_id}/locations/{agent_engine_resource_location}/reasoningEngines/{agent_engine_resource_id}"
-base_url = f"https://{agent_engine_resource_location}-aiplatform.googleapis.com/v1/{reasoning_engine_resource_id}"
-
-
-# def get_identity_token():
-#     credentials, _ = google_auth.default()
-#     auth_request = google_requests.Request()
-#     credentials.refresh(auth_request)
-
-#     return credentials.token
 
 
 @on_document_written(
     document="wizard/{auroraSessionId}", region="europe-west1", memory=512
 )
 def onAuroraEvents(event: Event[Change[DocumentSnapshot]]) -> None:
-    print(f"new file written: '{event.params["auroraSessionId"]}'")
-    print(f"value: {event.data}")
-    # print(event.data)
-    # print(event.data.params)
+    print(
+        f"new file written in '{event.params["auroraSessionId"]}': {event.data.after.to_dict()}"
+    )
 
-    processRequest(event.params["auroraSessionId"])
+    status = event.data.after.get("status")
+
+    if status != "start_matching":
+        print(f"status '{status}' will not trigger aurora. Exiting...")
+        return
+
+    auroraSessionId = event.params["auroraSessionId"]
+    demand_description = event.data.after.get("demandDescription")
+    sources_internal = event.data.after.get("sources.internal")
+    sources_external = event.data.after.get("sources.additional")
+
+    prompt_message = create_prompt_message(
+        auroraSessionId,
+        demand_description,
+        sources_internal,
+        sources_external,
+    )
+
+    processRequest(auroraSessionId, prompt_message)
 
 
-def processRequest(auroraSessionId):
+def create_prompt_message(
+    auroraSessionId, demandDescription, useInternalSources, useExternalSources
+):
+    return (
+        f"Research on the following demand: {demandDescription}."
+        f"Use internal sources: {useInternalSources}, external sources: {useExternalSources}."
+        f"session id: '{auroraSessionId}'."
+    )
+
+
+def get_engine():
     print("Triggering agent engine...")
 
     vertexai.init(
@@ -58,44 +65,35 @@ def processRequest(auroraSessionId):
     all_engines = agent_engines.list(filter=f'display_name="{agent_engine_app_name}"')
     engine = next(all_engines)
 
+    return engine
+
+
+def processRequest(sessionId, prompt_message):
+    engine = get_engine()
+
     print(f"Using agent engine app: {engine.display_name}")
 
-    stream_query(engine, auroraSessionId)
+    stream_query(engine, sessionId, prompt_message)
 
 
-def stream_query(engine, auroraSessionId):
-    print(f"stream query for session: {auroraSessionId}")
+def stream_query(engine, sessionId, prompt_message):
+    print(f"stream query for session: {sessionId}")
+
+    print(f"query with message: {prompt_message}")
 
     events = engine.stream_query(
-        user_id=f"{auroraSessionId}",
-        # session_id=f"{auroraSessionId}",  # Optional
-        message=f"There is a new request in firestore in 'wizard/{auroraSessionId}'. Please process it.",
+        user_id=f"{sessionId}",
+        message=prompt_message,
     )
 
     for event in events:
         print(f"event: {event}")
 
-    # max_events = 3
-    # for i in range(max_events):
-    #     try:
-    #         event = next(events)
-    #         print(f"event: {event}")
-    #     except StopIteration:
-    #         print("No more events.")
-    #         break
-
-    # for event in engine.stream_query(
-    #     user_id=f"{auroraSessionId}",
-    #     # session_id=f"{auroraSessionId}",  # Optional
-    #     message="There is a new request in firestore in 'wizard/{auroraSessionId}'. Please process it.",
-    # ):
-    #     print(f"event: {event}")
-
 
 # main is only used for local debugging
 def main():
     print("starting function code in local debug mode")
-    processRequest("debug")
+    processRequest("hj2wWIrrxSRxAEIllmWH")
 
 
 if __name__ == "__main__":
